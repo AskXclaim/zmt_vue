2066986c850efedc958af39a9fc2bc579d1ca87b
Kola <ask_kol@yaoo.co.uk>
2023-10-04 15:01:39 +0100

    Add class & style binding exercises

---
+++ vue_more_fundamentals/app.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            textColor: "white",
+            circleSize: 200,
+            // circleFontSize:30
+        }
+    },
+    computed: {
+        circleFontSize() {
+            return (this.circleSize / 6.5);
+        },
+        circleRotationDeg() {
+            return (this.circleSize - 200) * 3.6;
+        }
+    }
+}).mount("#app");
