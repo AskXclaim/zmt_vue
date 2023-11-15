8165da8a39ceab1edf3c968d30d9090e2204ce38
Kolapo Abolarinwa <laughter@Kolapos-MacBook-Pro.local>
2023-11-15 16:16:24 +0000

    use tailwindcss styles instead for some menu items

--- vue_music/music/src/components/music_app/MusicAppMenuBar.vue
+++ vue_music/music/src/components/music_app/MusicAppMenuBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-row items-center pl-20 text-white bg-slate-700 h-24">
-    <div class="font-bold text-2xl">Music</div>
+    <div class="font-bold text-2xl cursor-pointer hover:font-extrabold hover:text-3xl">Music</div>
     <div class="font-thin">
-      <ul class="list-none">
-        <li>Login/Register</li>
-        <li>Manage</li>
+      <ul class="list-none cursor-pointer">
+        <li class="hover:font-bold">Login/Register</li>
+        <li class="hover:font-bold">Manage</li>
       </ul>
     </div>
   </div>
@@ -20,10 +20,5 @@ ul li {
   display: inline-block;
   padding: 5px;
   margin: 5px;
-  cursor: pointer;
-
-  &:hover {
-    font-weight: bolder;
-  }
 }
 </style>
